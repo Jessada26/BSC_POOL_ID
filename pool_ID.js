f7cd52cc99ace8fd3dad00756756b094ce54c10c
Jessada26 <inpangxm@gmail.com>
2021-09-13 15:38:29 +0700

    change count to startClickID;2C

--- pool_ID.js
+++ pool_ID.js
@@ -1,10 +1,10 @@
+let startClickID = 0;
 let yourLpInput = "0x01ecc44Ddd2D104F44D2AA1A2bD9DFbC91aE8275";
 let poolInfo = "9";
 
 let btn = document.querySelector(
   "#readCollapse" + String(poolInfo) + " #btn_" + String(poolInfo)
 );
-let count = 0;
 
 async function Auto() {
   if (
@@ -12,25 +12,25 @@ async function Auto() {
       document.querySelector("#myanswer_" + String(poolInfo) + " a").innerText
     ) === String(yourLpInput)
   ) {
-    if (Number(count - 1) === -1) {
+    if (Number(startClickID - 1) === -1) {
       await alert(
         "poolID is:" +
           document.querySelector("#readCollapse" + String(poolInfo) + " input")
             .value
       );
     } else {
-      await alert("poolID is:" + (count - 1));
+      await alert("poolID is:" + (startClickID - 1));
     }
   } else {
     document.querySelector(
       "#readCollapse" + String(poolInfo) + " input"
-    ).value = await count;
+    ).value = await startClickID;
     await btn.click();
     if (btn.disabled) {
-      await count++;
+      await startClickID++;
       await setTimeout(Auto, 1500);
     } else {
-      await alert("BSC Limit click at pool_id:" + (count - 1));
+      await alert("BSC Limit click at pool_id:" + (startClickID - 1));
     }
   }
 }
