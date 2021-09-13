2e0e5eb1bd141350506f0c1b2744976d6c0961ec
Jessada26 <inpangxm@gmail.com>
2021-09-13 15:34:32 +0700

    fixed -1 count bug and solve rule of BSC Limit clicked

--- pool_ID.js
+++ pool_ID.js
@@ -1,17 +1,38 @@
-let yourLpInput = '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6';  
-let poolInfo = '9';
+let yourLpInput = "0x01ecc44Ddd2D104F44D2AA1A2bD9DFbC91aE8275";
+let poolInfo = "9";
 
-let btn = document.querySelector("#readCollapse"+String(poolInfo)+" #btn_"+String(poolInfo));
+let btn = document.querySelector(
+  "#readCollapse" + String(poolInfo) + " #btn_" + String(poolInfo)
+);
 let count = 0;
 
 async function Auto() {
-     if (String(document.querySelector("#myanswer_"+String(poolInfo)+" a").innerText)===String(yourLpInput)) {
-       await alert("poolID is:"+(count-1))
-     } else {
-     document.querySelector("#readCollapse"+String(poolInfo)+" input").value = await count;
-     await btn.click();
-     if (btn.disabled) await count++;
-     await setTimeout(Auto,1400);
-  }
+  if (
+    String(
+      document.querySelector("#myanswer_" + String(poolInfo) + " a").innerText
+    ) === String(yourLpInput)
+  ) {
+    if (Number(count - 1) === -1) {
+      await alert(
+        "poolID is:" +
+          document.querySelector("#readCollapse" + String(poolInfo) + " input")
+            .value
+      );
+    } else {
+      await alert("poolID is:" + (count - 1));
     }
-    Auto();
+  } else {
+    document.querySelector(
+      "#readCollapse" + String(poolInfo) + " input"
+    ).value = await count;
+    await btn.click();
+    if (btn.disabled) {
+      await count++;
+      await setTimeout(Auto, 1500);
+    } else {
+      await alert("BSC Limit click at pool_id:" + (count - 1));
+    }
+  }
+}
+
+Auto();
